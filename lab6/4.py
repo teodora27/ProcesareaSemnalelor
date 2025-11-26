1fc091cd4234616f2f48703d0cb5e4980e1ba21d
teodora27 <cojocaruteodoralaura@yahoo.com>
2025-11-26 14:27:17 +0200

    update

--- lab6/4.py
+++ lab6/4.py
@@ -12,10 +12,22 @@ y = np.roll(x, d)
 X = np.fft.fft(x)
 Y = np.fft.fft(y)
 
+print(X)
+print(Y)
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 6))
+# axs[0].plot(Y)
+# axs[1].plot(X)
+axs[0].plot(np.abs(X))  
+axs[1].plot(np.abs(Y))  
+
+plt.tight_layout()
+plt.show()
+
 r0 = np.fft.ifft(X * np.conj(Y)).real
 d0 = np.argmax(r0)
 
-r1 = np.fft.ifft(X * Y).real
+r1 = np.fft.ifft(np.conj(X) * Y).real
 d1 = np.argmax(r1)
 
 r2 = np.fft.ifft(Y / X).real
@@ -26,11 +38,12 @@ print("Deplasarea reală d =", d)
 
 print("\nRezultat metoda 0: " + str(d0))
 print(np.round(r0, 3))
-# d0 reprezinta cu cat trebuie de[plasat y ca sa devina x
+# d0 reprezinta cu cat trebuie deplasat y ca sa devina x
 # din acest motiv se afiseaza 18 si nu 2
 
 print("\nRezultat metoda 1: " + str(d1))
 print(np.round(r1, 3))
+# d1 reprezinta cu cat trebuie deplasat x ca sa devina y
 
 print("\nRezultat metoda 2:"+ str(d2))
 print(np.round(r2, 3))
